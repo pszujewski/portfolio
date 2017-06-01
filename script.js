6d97996f02fc5a2e8406ce5818e732589338f4cf
Peter Szujewski <pszujewski@gmail.com>
2017-06-01 12:07:04 -0400

    Completed js features and code

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
-  // Smooth Scrolling Snippet from CSS Tricks
-  // https://css-tricks.com/snippets/jquery/smooth-scrolling/
-  //'a[href*="#"]:not([href="#"])'
-function scroll(selector) {
-    $(selector).on('click', function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
+function animateScroll(window, hideme) {
+  // have an initial evaluation in the event of a refresh...
+  let aboutMeIsVisible = false;
+  window.scroll(function(){
+    /* Check the location of each desired element */
+    hideme.each( function(element) {
+
+        const el = $(this);
+        const elClass = el.attr("class");
+        const isAboutMe = elClass.indexOf('about-me') > -1;
+
+        if (isAboutMe && !aboutMeIsVisible || !isAboutMe && aboutMeIsVisible) {
+
+          const object_target_position = el.position().top + 150;
+
+          const bottom_of_window = window.scrollTop() + window.height();
+
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > object_target_position ) {
+
+            el.animate({'opacity':'1'},500);
+
+            if (isAboutMe) {
+              aboutMeIsVisible = true;
+            }
+
+            else {
+              window.unbind('scroll');
+            }
+
+          }
+
+        }  
+
     });
-  });  
+
+  }); 
+
+}
+
+function handleScrollToTop(window, anchor) {
+  anchor.on('click', function(e) {
+    window.scrollTo(0, 1500);
+  });
+}
 
 $(document).ready(function(){
-  scroll('a[href*="#"]:not([href="#"])');
+
+  const windowGlobal = $(window);
+
+  animateScroll(windowGlobal, $('.hideme'));
+
+  handleScrollToTop(windowGlobal, $('.js-go-to-top'));
+
 });
