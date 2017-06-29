c08b9bd833ecc8846b5822cc26bc3a5fbe4ea2d0
Peter Szujewski <pszujewski@gmail.com>
2017-06-29 10:56:48 -0400

    Removes scrolling animation and fade in to reduce complexity

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-function animateScroll(window, hideme) {
-  // have an initial evaluation in the event of a refresh...
-  let aboutMeIsVisible = false;
-  window.scroll(function(){
-    /* Check the location of each desired element */
-    hideme.each( function(element) {
-
-        const el = $(this);
-        const elClass = el.attr("class");
-        const isAboutMe = elClass.indexOf('about-me') > -1;
-
-        if (isAboutMe && !aboutMeIsVisible || !isAboutMe && aboutMeIsVisible) {
-
-          const object_target_position = el.position().top + 50;
-
-          const bottom_of_window = window.scrollTop() + window.height();
-
-          /* If the object is completely visible in the window, fade it it */
-          if( bottom_of_window > object_target_position ) {
-
-            el.animate({'opacity':'1'},500);
-
-            if (isAboutMe) {
-              aboutMeIsVisible = true;
-            }
-
-            else {
-              window.unbind('scroll');
-            }
-
-          }
-
-        }  
-
-    });
-
-  }); 
-
-}
-
 function handleScrollToTop(window, anchor) {
   anchor.on('click', function(e) {
     window.scrollTo(0, 1500);
@@ -47,8 +7,6 @@ function handleScrollToTop(window, anchor) {
 $(document).ready(function(){
 
   const windowGlobal = $(window);
-
-  animateScroll(windowGlobal, $('.hideme'));
 
   handleScrollToTop(windowGlobal, $('.js-go-to-top'));
 
